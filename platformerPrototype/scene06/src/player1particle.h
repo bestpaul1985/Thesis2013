7c0c6214e264cb66c604ad21497aaea26ffb135a
Tharit Tothong <litchirhythm@gmail.com>
2012-12-04 08:09:43 -0500

    copy over from scene 5

---
+++ platformerPrototype/scene06/src/player1particle.h
@@ -0,0 +1,22 @@
+//
+//  player1particle.h
+//  scense05
+//
+//  Created by PengCheng on 12/3/12.
+//
+//
+
+#ifndef __scense05__player1particle__
+#define __scense05__player1particle__
+
+#include "ofMain.h"
+#include "particle.h"
+
+class player1particle : public particle{
+
+public:
+    void draw();
+
+};
+
+#endif /* defined(__scense05__player1particle__) */
