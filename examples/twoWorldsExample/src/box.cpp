b9603eabc4fe8cefa7409781e61659e00a97eb4c
Tharit Tothong <litchirhythm@gmail.com>
2013-03-01 13:03:06 -0500

    push my tries which didn't go very far T___T

---
+++ examples/twoWorldsExample/src/box.cpp
@@ -0,0 +1,9 @@
+//
+//  box.cpp
+//  twoWorldExample
+//
+//  Created by Firm Read on 3/1/13.
+//
+//
+
+#include "box.h"
---
+++ examples/twoWorldsExample/src/box.h
@@ -0,0 +1,14 @@
+//
+//  box.h
+//  twoWorldExample
+//
+//  Created by Firm Read on 3/1/13.
+//
+//
+
+#ifndef __twoWorldExample__box__
+#define __twoWorldExample__box__
+
+#include <iostream>
+
+#endif /* defined(__twoWorldExample__box__) */
