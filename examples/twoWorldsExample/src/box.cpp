728f70636194c0b61aa79dc847ca30668736b1d6
Tharit Tothong <litchirhythm@gmail.com>
2013-03-01 13:03:18 -0500

    another stuffs

--- examples/twoWorldsExample/src/box.cpp
+++ examples/twoWorldsExample/src/box.cpp
@@ -7,3 +7,38 @@
 //
 
 #include "box.h"
+
+void box::setup(ofxBox2d world1, ofxBox2d world2){
+    
+    // we can also have a vector of any shape that is
+    // shared between both worlds
+    for(int i=0; i<20; i++) {
+        
+        ofxBox2dRect r;
+        r.setPhysics(1, 0.7, 0.9);
+        
+        // add to world A
+        if(i <= 9) {
+            r.setup(world1.getWorld(), 250+ofRandom(-50, 50), 10, ofRandom(10,30), ofRandom(10,30));
+        }
+        
+        // add to world B
+        else {
+            r.setup(world2.getWorld(), 750+ofRandom(-50, 50), 10, ofRandom(10,30), ofRandom(10,30));
+        }
+        
+        // add to one vector
+        sharedRects.push_back(r);
+    }
+}
+
+
+void box::draw(){
+    // Shared Rects
+    for (int i=0; i<sharedRects.size(); i++) {
+        ofSetHexColor(0x2F9BA1);
+        ofFill();
+        sharedRects[i].draw();
+    }
+    
+}
--- examples/twoWorldsExample/src/box.h
+++ examples/twoWorldsExample/src/box.h
@@ -10,5 +10,17 @@
 #define __twoWorldExample__box__
 
 #include <iostream>
+#include "ofMain.h"
+#include "ofxBox2d.h"
+
+class box{
+public:
+    void setup(ofxBox2d world1, ofxBox2d world2);
+//    void update();
+    void draw();
+    
+    vector <ofxBox2dRect>    sharedRects;
+};
+
 
 #endif /* defined(__twoWorldExample__box__) */
