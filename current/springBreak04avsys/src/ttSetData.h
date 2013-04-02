b6b2db8f951c168056cca1f0ae5a89bb0897860f
Tharit Tothong <litchirhythm@gmail.com>
2013-04-02 05:30:35 -0400

    copy to make avsys sound visualizer version

---
+++ current/springBreak04avsys/src/ttSetData.h
@@ -0,0 +1,20 @@
+//
+//  ropeData.h
+//  ttContactListener
+//
+//  Created by PengCheng on 3/16/13.
+//
+//
+
+#ifndef __ttContactListener__ttSetData__
+#define __ttContactListener__ttSetData__
+
+#include "ofMain.h"
+class ttSetData{
+public:
+    string name;
+	bool bHit;
+    bool bHook;
+};
+
+#endif /* defined(__ttContactListener__ttSetData__) */
