4143ab31dcbb6b3589e856708b58ca5e6648f241
Tharit Tothong <litchirhythm@gmail.com>
2013-04-15 18:06:47 -0400

    afterSpringSprite = copied version of afterSpring01

---
+++ current/afterSpringSprite/src/ttRope.h
@@ -0,0 +1,61 @@
+//
+//  ttRope.h
+//  springBreak02
+//
+//  Created by PengCheng on 3/28/13.
+//
+//
+
+#ifndef __springBreak02__ttRope__
+#define __springBreak02__ttRope__
+
+#include "ofMain.h"
+#include "ofxBox2d.h"
+#include "ttControl.h"
+class ttRope{
+public:
+    void setup(ofPoint &accFrc,ofPoint &_screenA,ofPoint &_screenB, ofPoint &_CharA, ofPoint &_CharB,ttControl &cont, int num);
+    void update();
+    void send();
+    void initialize(ofPoint pos);
+    void destroy();
+    void draw();
+    
+    ofxBox2d world;
+    ofxBox2dRect start;
+    ofxBox2dRect end;
+    ofxBox2dJoint joint;
+    
+    vector<ofxBox2dRect>    rects;
+    vector<b2RevoluteJoint*> joints;
+    
+    ofPoint *acc;
+    ofPoint *screenA;
+    ofPoint *screenB;
+    ofPoint *charA;
+    ofPoint *charB;
+    ttControl *control;
+    
+    float length;
+    float startTime;
+    float duration;
+    
+    bool bHooked;
+    bool bReady;
+    bool bRopeInUse;
+    bool bInitialize;
+    bool bFall;
+    bool bInSky;
+    bool bSwing;
+    int counter;
+    int ropeNum;
+    int m_num;
+    int m_preNum;
+    int closetRect;
+    
+    float rectW;
+    float rectH;
+    float rectOff;
+};
+
+#endif /* defined(__springBreak02__ttRope__) */
