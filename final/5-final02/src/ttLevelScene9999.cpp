13d51518d59711e273eb76d8779737b89cd89a95
Peng Cheng <bestpaul1985@gmail.com>
2013-05-04 20:13:23 -0400

    bad

---
+++ final/5-final02/src/ttLevelScene9999.cpp
@@ -0,0 +1,71 @@
+//
+//  ttLevelScene01.cpp
+//  final02
+//
+//  Created by PengCheng on 5/4/13.
+//
+//
+
+#include "ttLevelScene01.h"
+
+void ttLevelScene01::setup(int &CurrentScene){
+
+}
+//--------------------------------------------------------------
+void ttLevelScene01::update(){
+
+}
+//--------------------------------------------------------------
+void ttLevelScene01::draw(){
+
+}
+//--------------------------------------------------------------
+void ttLevelScene01::drawScene(int iCameraDraw){
+
+}
+//--------------------------------------------------------------
+void ttLevelScene01::touchDown(ofTouchEventArgs &touch){
+
+}
+//--------------------------------------------------------------
+void ttLevelScene01::touchMoved(ofTouchEventArgs &touch){
+
+}
+//--------------------------------------------------------------
+void ttLevelScene01::touchUp(ofTouchEventArgs &touch){
+
+}
+//--------------------------------------------------------------
+void ttLevelScene01::contactStart_worldA(ofxBox2dContactArgs &e){
+
+}
+//--------------------------------------------------------------
+void ttLevelScene01::contactEnd_worldA(ofxBox2dContactArgs &e){
+
+}
+//--------------------------------------------------------------
+void ttLevelScene01::contactStart_worldB(ofxBox2dContactArgs &e){
+    
+}
+//--------------------------------------------------------------
+void ttLevelScene01::contactEnd_worldB(ofxBox2dContactArgs &e){
+    
+}
+//--------------------------------------------------------------
+void ttLevelScene01::position(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
