69dd67b301935225a2dd416cdc3e6627902edfc4
Peng Cheng <bestpaul1985@gmail.com>
2013-05-05 19:15:58 -0400

    Revert "Revert "Revert "Revert "Revert "bad"""""
    This reverts commit 08709f87d33da61e72d507ecff4d5e7e3ef03cdf.

--- final/6-final03/src/testApp.mm
+++ final/6-final03/src/testApp.mm
@@ -4,129 +4,94 @@
 void testApp::setup(){
     
 	iPhoneSetOrientation(OFXIPHONE_ORIENTATION_LANDSCAPE_LEFT);
-    scenes.push_back(new openingScene());
+    
+	scenes[0] = new openingScene();
+    scenes[1] = new springScene();
+	scenes[4] = new squareScene();
+	scenes[2] = new circleScene();
+	scenes[3] = new imageScene();
+
+	currentScene = 0;
 	
-    currentScene = 0;
-    loader = L_LEVEL02;
-    rect[0].set(100, 100, 100, 100);
-    rect[1].set(400, 100, 100, 100);
+	for (int i = 0; i < SCENE_NUMBER; i++){
+		scenes[i]->setup();
+        scenes[i]->goToScene = i+1;
+        scenes[4]->goToScene = 0;
+	}
+    
+	
 }
 //--------------------------------------------------------------
 void testApp::update(){
-	
-    switch (loader) {
-        case L_NONE:{
-            scenes[0]->update();
-        }break;
-        case L_MAIN:{
-            delete scenes[0];
-            scenes.push_back(new level01());
-            scenes->setup();
-            if (scenes->SETUP == true) {
-                loader = L_NONE;
-                currentScene = 0;
-            }
-        }break;
-            
-        case L_LEVEL01:{
-            delete scenes[0];
+	scenes[currentScene]->update();
     
-            scenes.push_back(new level02());
-            scenes->setup();
-            if (scenes->SETUP == true) {
-                currentScene = 1;
-                loader = L_NONE;
-            }
-            
-        }break;
-        case L_LEVEL02:{
-            delete scenes;
-            scenes= NULL;
-            scenes = new level02();
-            scenes->setup();
-            if (scenes->SETUP == true) {
-                currentScene = 2;
-                loader = L_NONE;
-            }
-        }break;
+    for (int i = 0; i < SCENE_NUMBER; i++){
+        if (scenes[i]->end == true) {
+            currentScene=scenes[i]->goToScene;
+            scenes[i]->end = false;
+        }
     }
-    
+
 }
 //--------------------------------------------------------------
 void testApp::draw(){
-    switch (loader) {
-        case L_NONE:{
-            scenes->draw();
-            ofSetColor(255, 255, 255);
-            ofRect(rect[0]);
-            ofRect(rect[1]);
-        }break;
-        case L_MAIN:{
-            
-        }break;
-        case L_LEVEL01:{
-            
-        } break;
-        case L_LEVEL02:{
-            
-        }break;
-    
-    }
+	scenes[currentScene]->draw();
 }
 //--------------------------------------------------------------
 void testApp::exit(){
+
 }
+
 //--------------------------------------------------------------
 void testApp::touchDown(ofTouchEventArgs & touch){
+    
+    
    
-    scenes->touchDown(touch);
+        currentScene++;
+        currentScene %= SCENE_NUMBER;
     
+	
+//	((circleScene*)scenes[1])->sayHello();
+    scenes[currentScene]->touchDown(touch);
     
-    if(rect[0].inside(touch.x,touch.y)){
-        loader = L_LEVEL02;
-    }
-
-    if(rect[1].inside(touch.x,touch.y)){
-        loader = L_LEVEL01;
-    }
 }
 
 //--------------------------------------------------------------
 void testApp::touchMoved(ofTouchEventArgs & touch){
-    scenes->touchMoved(touch);
+    scenes[currentScene]->touchMoved(touch);
 }
 
 //--------------------------------------------------------------
 void testApp::touchUp(ofTouchEventArgs & touch){
-    scenes->touchUp(touch);
+    scenes[currentScene]->touchUp(touch);
 }
 
 //--------------------------------------------------------------
 void testApp::touchDoubleTap(ofTouchEventArgs & touch){
-    scenes->touchDoubleTap(touch);
+    scenes[currentScene]->touchDoubleTap(touch);
 }
 
 //--------------------------------------------------------------
 void testApp::touchCancelled(ofTouchEventArgs & touch){
-    scenes->touchCancelled(touch);
+    scenes[currentScene]->touchCancelled(touch);
 }
 
 //--------------------------------------------------------------
 void testApp::lostFocus(){
-    scenes->lostFocus();
+    scenes[currentScene]->lostFocus();
 }
 
 //--------------------------------------------------------------
 void testApp::gotFocus(){
-    scenes->gotFocus();
+    scenes[currentScene]->gotFocus();
 }
 
 //--------------------------------------------------------------
 void testApp::gotMemoryWarning(){
-    scenes->gotMemoryWarning();
+    scenes[currentScene]->gotMemoryWarning();
 }
 
 //--------------------------------------------------------------
 void testApp::deviceOrientationChanged(int newOrientation){
-    scenes->deviceOrientationChanged(newOrientation);
+    scenes[currentScene]->deviceOrientationChanged(newOrientation);
 }
